c251e25eeff40c8112df36d89dd1a0365978b5f7
Yossif Alali <YossifAlali01@hotmail.com>
2025-03-10 15:26:41 +0300

    added union

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-x = 12233
+set_A = {1,2,3,4,5}
+set_B = {6,7,8,9,10}
+
+# set is a collection of unique elements, the order when we input the elements is not maintained, and the elements are not indexed
+
+print(set_A | set_B)
