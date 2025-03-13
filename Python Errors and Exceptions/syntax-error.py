f28a331988c3d7dcb16ce94c2fd1504689394871
Yossif Alali <YossifAlali01@hotmail.com>
2025-03-13 12:59:16 +0300

    updated syntax-error.py

--- Python Errors and Exceptions/syntax-error.py
+++ Python Errors and Exceptions/syntax-error.py
@@ -1,6 +1,16 @@
-def function()
+def function():
     print('hello')
 
 function()
+
+'''
+deff function()
+    print('hello)
+
+function()
+
+'''
+
+
 
 # produces an error by not following the syntax and writing rules of the PL, easy to spot and correct
